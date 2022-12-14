1fc5845bd7d58777cd05e0bac08dfa91801e01ba
avmanyc <avmanyc@gmail.com>
2022-12-14 08:14:32 -0500

    unpacking dictionaries

--- main.py
+++ main.py
@@ -3,34 +3,42 @@ with open("players.json", "r") as read_file:
     data = json.load(read_file)
     # print(data)
 
+# team_name = data['response'][0]['team']['name']
+# team_logo = data['response'][0]['team']['logo']
+# player_name = data['response'][1]['players'][0]['player']['name']
+# player_photo = data['response'][1]['players'][0]['player']['photo']
+# player_position = data['response'][1]['players'][0]['statistics'][0]['games']['position']
+# off_sides = data['response'][1]['players'][0]['statistics'][0]['offsides']
+# total_shots = data['response'][1]['players'][0]['statistics'][0]['shots']['total']
+# ontarget_shots = data['response'][1]['players'][0]['statistics'][0]['shots']['on']
+# # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# goals = data['response'][1]['players'][0]['statistics'][0]['goals']
+# passes = data['response'][1]['players'][0]['statistics'][0]['passes']
+# tackles = data['response'][1]['players'][0]['statistics'][0]['tackles']
+# duels = data['response'][1]['players'][0]['statistics'][0]['duels']
+# dribbles = data['response'][1]['players'][0]['statistics'][0]['dribbles']
+# fouls = data['response'][1]['players'][0]['statistics'][0]['fouls']
+# cards = data['response'][1]['players'][0]['statistics'][0]['cards']
+# penalty = data['response'][1]['players'][0]['statistics'][0]['penalty']
 
-team_name = data['response'][0]['team']['name']
-team_logo = data['response'][0]['team']['logo']
-player_name = data['response'][1]['players'][0]['player']['name']
-player_photo = data['response'][1]['players'][0]['player']['photo']
-player_position = data['response'][1]['players'][0]['statistics'][0]['games']['position']
-off_sides = data['response'][1]['players'][0]['statistics'][0]['offsides']
-total_shots = data['response'][1]['players'][0]['statistics'][0]['shots']['total']
-ontarget_shots = data['response'][1]['players'][0]['statistics'][0]['shots']['on']
-# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 goals = data['response'][1]['players'][0]['statistics'][0]['goals']
-passes = data['response'][1]['players'][0]['statistics'][0]['passes']
-tackles = data['response'][1]['players'][0]['statistics'][0]['tackles']
-duels = data['response'][1]['players'][0]['statistics'][0]['duels']
-dribbles = data['response'][1]['players'][0]['statistics'][0]['dribbles']
-fouls = data['response'][1]['players'][0]['statistics'][0]['fouls']
-cards = data['response'][1]['players'][0]['statistics'][0]['cards']
-penalty = data['response'][1]['players'][0]['statistics'][0]['penalty']
+print(goals)
+print(type(goals))
+
+for goal in goals:
+    for key, value in goals.items():
+        print(key)
+        print(value)
 
 
-print('goals: ', goals)
-print('passes: ', passes)
-print('tackles: ', tackles)
-print('duels: ', duels)
-print('dribbles: ', dribbles)
-print('fouls: ', fouls)
-print('cards: ', cards)
-print('penalty: ', penalty)
+# print('goals: ', goals)
+# print('passes: ', passes)
+# print('tackles: ', tackles)
+# print('duels: ', duels)
+# print('dribbles: ', dribbles)
+# print('fouls: ', fouls)
+# print('cards: ', cards)
+# print('penalty: ', penalty)
 
 
 
